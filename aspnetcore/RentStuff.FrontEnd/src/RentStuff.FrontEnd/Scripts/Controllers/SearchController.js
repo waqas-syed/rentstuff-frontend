95e1fc32f316d530d39915c3751981dcadf61f94
waqas_syed <waqas.syed@hotmail.com>
2016-12-07 10:49:15 +0500

    Get propertyTypes : INCOMPLETE

--- aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/Scripts/Controllers/SearchController.js
+++ aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/Scripts/Controllers/SearchController.js
@@ -1,13 +1,15 @@
 ﻿var rentApp = angular.module('rentApp');
 
 rentApp.controller('SearchController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
-    //$window.alert("Adios!!!");
-    var config = {email:"somene@someplace.com"};
-    $http.get('http://localhost:2431/v1/house', config).then(function successCallback(response) {
-        // this callback will be called asynchronously when the response is available
-        $window.alert('Http Get success');
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs or server returns response with an error status.
-        $window.alert('Http Get failure');
-    })
+
+    $scope.getPropertyTypes = function (value) {
+        //var config = { email: "somene@someplace.com" };
+        $http.get('http://localhost:2431/v1/property-types', config).then(function successCallback(response) {
+            // this callback will be called asynchronously when the response is available
+            $window.alert('Http Get success');
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs or server returns response with an error status.
+            $window.alert('Http Get failure');
+        })
+    }
 }]);
