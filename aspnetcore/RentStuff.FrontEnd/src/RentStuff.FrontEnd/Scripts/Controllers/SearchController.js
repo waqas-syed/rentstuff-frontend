8a7a51235f452dc6b1bfc72a8149a662ffb60bb4
waqas_syed <waqas.syed@hotmail.com>
2016-12-10 14:39:50 +0500

    Autocomplete: YAYYYYYYY!!!!!!

--- aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/Scripts/Controllers/SearchController.js
+++ aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/Scripts/Controllers/SearchController.js
@@ -1,22 +1,15 @@
 ﻿var rentApp = angular.module('rentApp');
 
 rentApp.controller('SearchController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
-        $http
-            .get('http://localhost:2431/v1/property-types')
+    
+    $http.get('http://localhost:2431/v1/property-types')
             .then(function (response) {
-                $window.alert('Http Get success');
+                $scope.propertyTypes = response.data;
+                $scope.selectedPropertyType = "Property Type";
             }).catch(function (e) {
                 $window.alert('Http Get Failure');
                 throw e;
             }).finally(function () {
                 
             });
-    //var config = { email: "somene@someplace.com" };
-    /*$http.get('http://localhost:2431/v1/property-types').then(function successCallback(response) {
-        // this callback will be called asynchronously when the response is available
-        $window.alert('Http Get success');
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs or server returns response with an error status.
-        $window.alert('Http Get failure');
-    });*/
 }]);
