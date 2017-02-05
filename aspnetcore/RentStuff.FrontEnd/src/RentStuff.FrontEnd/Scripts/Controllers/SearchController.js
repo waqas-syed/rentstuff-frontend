4b989cbf9f5fc06998d040329cf9201b93336f56
waqas_syed <waqas.syed@hotmail.com>
2017-02-05 16:50:33 +0500

    'Rs' added before Rent + Autocomplete turned off + new validations for search criteria

--- aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/Scripts/Controllers/SearchController.js
+++ aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/Scripts/Controllers/SearchController.js
@@ -12,9 +12,15 @@ rentApp.controller('SearchController', ['$scope', '$window', '$http', 'searchSer
     }
     
     $scope.searchHouses = function () {
-        if ($scope.area === '' || $scope.area === undefined ||
-            $scope.selectedPropertyType === '' || $scope.selectedPropertyType === undefined) {
+        if (($scope.area.formatted_address === '' || $scope.area.formatted_address === undefined) &&
+            ($scope.selectedPropertyType === '' || $scope.selectedPropertyType === undefined)) {
             $window.alert("Please provide Location and PropertyType");
+        }
+        else if ($scope.area === '' || $scope.area === undefined) {
+            $window.alert("Please provide Location");
+        }
+        else if ($scope.selectedPropertyType === '' || $scope.selectedPropertyType === undefined) {
+            $window.alert("Please provide PropertyType");
         } else {
             searchService
                 .searchHousesByAddressAndPropertyType($scope.area.formatted_address, $scope.selectedPropertyType)
