6b57ef0e2153c9cca7c64dd159bd3509201b8279
waqas_syed <waqas.syed@hotmail.com>
2017-02-09 15:08:23 +0500

    Minor changes

---
+++ aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/Scripts/Controllers/HouseDetailsController.js
@@ -0,0 +1,19 @@
+﻿var rentApp = angular.module('rentApp');
+
+rentApp.controller('HouseDetailsController', ['$scope', function ($scope) {
+    $scope.myInterval = 3000;
+    $scope.slides = [
+      {
+          image: 'http://lorempixel.com/400/200/'
+      },
+      {
+          image: 'http://lorempixel.com/400/200/food'
+      },
+      {
+          image: 'http://lorempixel.com/400/200/sports'
+      },
+      {
+          image: 'http://lorempixel.com/400/200/people'
+      }
+    ];
+}]);
--- aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/wwwroot/app.js
+++ aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/wwwroot/app.js
@@ -2,26 +2,6 @@
     'use strict';
 
     var rentApp = angular.module('rentApp', ['ui.router', 'google.places', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
-    /*rentApp.config(["$stateProvider", "$urlRouterProvider",
-        function ($stateProvider, $urlRouterProvider) {
-
-        $urlRouterProvider.otherwise('/home');
-
-        $stateProvider
-        
-            // HOME STATES AND NESTED VIEWS ========================================
-            .state('home', {
-                url: '/home',
-                templateUrl: 'landing-page.html'
-            })
-
-            // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-            .state('login', {
-                url: '/login',
-                templateUrl: 'landing-page.html'
-            });
-
-        }]);*/
 
     rentApp.config(["$stateProvider", "$urlRouterProvider", "$httpProvider",
 		function ($stateProvider, $urlRouterProvider, $httpProvider) {
