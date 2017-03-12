5b6c622e0710716d56a9e5a39d871664299eb722
waqas_syed <waqas.syed@hotmail.com>
2017-03-12 17:51:56 +0500

    ResolveService added: relates to previous commit

---
+++ aspnetcore/RentStuff.FrontEnd/src/RentStuff.FrontEnd/Scripts/Services/ResolveService.js
@@ -0,0 +1,19 @@
+﻿var rentApp = angular.module('rentApp');
+
+rentApp.factory('resolveService', ['$http', function ($http) {
+    return {
+        getHouseDetails: function (houseId) {
+            return $http.get('http://localhost:2431/v1/house', { params: { houseId: '99c6ad06-0706-4e56-83c8-b39764302194' } })
+            .then(
+                    function (response) {
+                        return response.data;
+                    },
+                    function (errResponse) {
+                        console.error('Error while fetching users');
+                        return errResponse;
+                        //   return $q.reject(errResponse);
+                    }
+            );
+        }
+    };
+}]);
