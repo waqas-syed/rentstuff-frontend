3c111cf07462905425ccc78a308ff6f53d02ebb7
waqas_syed <waqas.syed@hotmail.com>
2017-04-06 19:45:39 +0500

    Time to shake things up again; restructuring the project structure

--- RentStuff.FrontEnd/RentStuff.FrontEnd/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App.RentStuff.FrontEnd.Application" Language="C#" %>
--- RentStuff.FrontEnd/RentStuff.FrontEnd/Scripts/mainController.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-var myApp = angular.module('app', []);
-
-app.controller('MainController', ['$scope', '$window', function ($scope, $window) {
-        $scope.getLocation = function () {
-            $window.alert;
-        };
-}]);
