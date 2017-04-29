b8c4aeb628f75eaba1006c46aec05fb1db656d58
waqas_syed <waqas.syed@hotmail.com>
2017-04-29 16:58:37 +0500

    Carousel working except for the arrows showing up properly

--- src/RentStuff.FrontEnd/Scripts/Controllers/HouseDetailsController.js
+++ src/RentStuff.FrontEnd/Scripts/Controllers/HouseDetailsController.js
@@ -31,53 +31,14 @@ rentApp.controller('HouseDetailsController', ['$scope', '$state', '$stateParams'
         var slides = $scope.slides = [];
         var currIndex = 0;
 
-        $scope.addSlide = function (image) {
-            var newWidth = 600 + slides.length + 1;
+        $scope.addSlide = function (imageSrc) {
             slides.push({
-                //image: '//unsplash.it/' + newWidth + '/300',
-                image: image.src,
+                image: imageSrc,
                 id: currIndex++
             });
         };
 
-        $scope.randomize = function () {
-            var indexes = generateIndexesArray();
-            assignNewIndexesToSlides(indexes);
-        };
-
         for (var i = 0; i < houseDetails.imagesArray.length; i++) {
-            $scope.addSlide(houseDetails.imagesArray[i]);
-        }
-
-        // Randomize logic below
-
-        function assignNewIndexesToSlides(indexes) {
-            for (var i = 0, l = slides.length; i < l; i++) {
-                slides[i].id = indexes.pop();
-            }
-        }
-
-        function generateIndexesArray() {
-            var indexes = [];
-            for (var i = 0; i < currIndex; ++i) {
-                indexes[i] = i;
-            }
-            return shuffle(indexes);
-        }
-
-        // http://stackoverflow.com/questions/962802#962890
-        function shuffle(array) {
-            var tmp, current, top = array.length;
-
-            if (top) {
-                while (--top) {
-                    current = Math.floor(Math.random() * (top + 1));
-                    tmp = array[current];
-                    array[current] = array[top];
-                    array[top] = tmp;
-                }
-            }
-
-            return array;
+            $scope.addSlide(houseDetails.imagesArray[i].src);
         }
     }]);
