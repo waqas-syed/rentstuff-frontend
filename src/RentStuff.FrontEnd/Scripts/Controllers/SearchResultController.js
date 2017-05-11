f73b607c92abc2d85684ec7d30eb3b8286d3d1e2
waqas_syed <waqas.syed@hotmail.com>
2017-05-11 16:25:35 +0500

    Pagination: Sample demonstration working

--- src/RentStuff.FrontEnd/Scripts/Controllers/SearchResultController.js
+++ src/RentStuff.FrontEnd/Scripts/Controllers/SearchResultController.js
@@ -1,7 +1,7 @@
 ﻿var rentApp = angular.module('rentApp');
 
-rentApp.controller('SearchResultController', ['$scope', '$state', '$stateParams', 'searchService', 'authService',
-    function ($scope, $state, $stateParams, searchService, authService) {
+rentApp.controller('SearchResultController', ['$scope', '$state', '$stateParams', 'searchService', 'authService', 'pagerService',
+    function ($scope, $state, $stateParams, searchService, authService, pagerService) {
 
         var searchParameters = null;
         $scope.ownerViewingOwnProperties = false;
@@ -25,7 +25,7 @@ rentApp.controller('SearchResultController', ['$scope', '$state', '$stateParams'
                         // Location as parameter
                         searchParameters = { area: $stateParams.location };
                     }
-                } 
+                }
             }
         }
         searchService.searchHouses(searchParameters)
@@ -38,4 +38,33 @@ rentApp.controller('SearchResultController', ['$scope', '$state', '$stateParams'
         $scope.navigateToDetails = function(houseId) {
             $state.go('house-details', {houseId: houseId});
         };
+
+        var vm = $scope.vm = this;
+
+        var input = [];
+        for (var i = 0; i <= 151; i++) {
+            input.push(i);
+        }
+        vm.dummyItems = input; // dummy array of items to be paged
+        vm.pager = {};
+        vm.setPage = setPage;
+
+        initController();
+
+        function initController() {
+            // initialize to page 1
+            vm.setPage(1);
+        }
+
+        function setPage(page) {
+            if (page < 1 || page > vm.pager.totalPages) {
+                return;
+            }
+
+            // get pager object from service
+            vm.pager = pagerService.GetPager(vm.dummyItems.length, page);
+
+            // get current page of items
+            vm.items = vm.dummyItems.slice(vm.pager.startIndex, vm.pager.endIndex + 1);
+        }
 }]);
